84d06f0d8de31aba752d09a2ae8915204879143d
Grafscorp <golyandringraf@gmail.com>
2025-05-10 15:43:57 +0300

    Expanded SystemInfo by CpuInfo

--- include/SystemInfo.hpp
+++ include/SystemInfo.hpp
@@ -1,17 +1,27 @@
 #ifndef SYSTEMINFO_HPP
 #define  SYSTEMINFO_HPP
 
-
-
+#ifdef _WIN32
+#include <windows.h>
+#include <iomanip>
+#endif
+#include <CpuInfoL.hpp>
 
 namespace systemInfoL{
 
 //Object for get system information
-class SystemInfo
+class SystemInfo : public CpuInfo
 {
+public:
+    template<typename CPercentInfo> 
+    //Return CPU Load in Percent for any OS
+    CPercentInfo GetCpuPercent();
 
+    template<typename MPercentInfo> 
+    //Return RAM Load in Percent for any OS
+    MPercentInfo GetMemoryPercent();
 
-
+protected:
 
 };
 
--- src/SystemInfo.cpp
+++ src/SystemInfo.cpp
@@ -1,8 +1,22 @@
 #include <SystemInfo.hpp>
 
 namespace systemInfoL{
-     
+    template<typename CPercentInfo> 
+    CPercentInfo SystemInfo::GetCpuPercent()
+    {
+        #ifdef _WIN32
+            return GetCPUInfoWindows();
+        #endif
 
+        return 0.0;
+    }
+
+    
+    template<typename MPercentInfo> 
+    MPercentInfo SystemInfo::GetMemoryPercent()
+    {
+        return 0.0;
+    }
 
 
 }
